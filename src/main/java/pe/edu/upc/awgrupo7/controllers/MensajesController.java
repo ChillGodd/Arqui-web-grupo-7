e5a7a48f3872d328985a4ed0f62f8ed8b1091630
DxthMaX <maxalvacamino@gmail.com>
2024-09-05 22:32:22 -0500

    Inclusión de la entidad Mensajes

---
+++ src/main/java/pe/edu/upc/awgrupo7/controllers/MensajesController.java
@@ -0,0 +1,37 @@
+package pe.edu.upc.awgrupo7.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.awgrupo7.dtos.MensajesDTO;
+import pe.edu.upc.awgrupo7.entities.Mensajes;
+import pe.edu.upc.awgrupo7.servicesinterfaces.IMensajesService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/Mensajes")
+public class MensajesController {
+
+    @Autowired
+    private IMensajesService mS;
+
+    @PostMapping
+    public void insertar(@RequestBody MensajesDTO dto)
+    {
+        ModelMapper m = new ModelMapper();
+        Mensajes men = m.map(dto, Mensajes.class);
+        mS.insert(men);
+    }
+
+    @GetMapping
+    public List<MensajesDTO> listar() {
+
+        return mS.list().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, MensajesDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/pe/edu/upc/awgrupo7/servicesinterfaces/IMensajesService.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.awgrupo7.servicesinterfaces;
+
+import pe.edu.upc.awgrupo7.entities.Mensajes;
+
+import java.util.List;
+
+public interface IMensajesService {
+
+    public List<Mensajes> list();
+
+    public void insert(Mensajes men);
+}
